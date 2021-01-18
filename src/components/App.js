20bbc65b4f146c0bf55f1e869350bac881d98aa6
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-18 10:55:30 +0100

    refactor promise/.then chain to async/await syntax

--- src/components/App.js
+++ src/components/App.js
@@ -3,26 +3,23 @@ import axios from 'axios';
 import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-onSearchSubmit(term){
-  axios
-    .get("https://api.unsplash.com/search/photos", {
+  async onSearchSubmit(term) {
+    const response  = await axios.get("https://api.unsplash.com/search/photos", {
       params: { query: term },
       headers: {
         Authorization: "Client-ID BwlPrF09Xnaln3HNBar_9_8BImInXHfPhyjvn_lrtdk",
       },
-    })
-    .then((response) => {
-      console.log(response.data.results);
     });
-}
+    console.log(response.data.results);
+  }
 
   render() {
-     return (
-     <div className="ui container" style={{ marginTop: "10px" }}>
-       <SearchBar onSubmit={this.onSearchSubmit}/>
-     </div>
-   );
- }
+    return (
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+      </div>
+    );
+  }
 }
 
 export default App; 
