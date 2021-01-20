9e65653aecd6e7935870be36b0fdb06382028a1e
CathKolar <68386285+CathKolar@users.noreply.github.com>
2021-01-20 11:23:40 +0100

    clean up comments

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -3,9 +3,7 @@ import React from "react";
 
 const ImageList = (props) => {
   const images = props.images.map(({id, urls, description}) => {
-    return (
-      <img key={id} src={urls.regular} alt={description} />
-    ); //alt text under "description"
+    return <img key={id} src={urls.regular} alt={description} />;
   });
   return <div className="image-list">{images}</div>;
 };
